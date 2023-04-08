da26f0d2fe9b7c00757e6c1ee125dc3e72cb9b35
10Gaby <galvisgabriela1@gmail.com>
2023-04-07 23:38:36 -0500

    avances

--- js/popup.js
+++ js/popup.js
@@ -1,145 +1,17 @@
-//Pop UP
+function generateModalListeners() {
+  for (let i = 1; i <= 30; i++) {
+    const open = document.getElementById(`open${i}`);
+    const modal_container = document.getElementById(`modal_container${i}`);
+    const close = document.getElementById(`close${i}`);
 
- //Modal 1
- const open1 = document.getElementById('open1');
- const modal_container1 = document.getElementById('modal_container1');
- const close1 = document.getElementById('close1');
- 
- open1.addEventListener('click', () => {
-   modal_container1.classList.add('show');  
- });
- 
- close1.addEventListener('click', () => {
-   modal_container1.classList.remove('show');
- });
- 
- 
- //Modal 2
- const open2 = document.getElementById('open2');
- const modal_container2 = document.getElementById('modal_container2');
- const close2 = document.getElementById('close2');
- 
- open2.addEventListener('click', () => {
-   modal_container2.classList.add('show');  
- });
- 
- close2.addEventListener('click', () => {
-   modal_container2.classList.remove('show');
- });
- 
- 
- //Modal 3
- const open3 = document.getElementById('open3');
- const modal_container3 = document.getElementById('modal_container3');
- const close3 = document.getElementById('close3');
- 
- open3.addEventListener('click', () => {
-   modal_container3.classList.add('show');  
- });
- 
- close3.addEventListener('click', () => {
-   modal_container3.classList.remove('show');
- });
- 
- 
- //Modal 4
- const open4 = document.getElementById('open4');
- const modal_container4 = document.getElementById('modal_container4');
- const close4 = document.getElementById('close4');
- 
- open4.addEventListener('click', () => {
-   modal_container4.classList.add('show');  
- });
- 
- close4.addEventListener('click', () => {
-   modal_container4.classList.remove('show');
- });
- 
- 
- //Modal 5
- const open5 = document.getElementById('open5');
- const modal_container5 = document.getElementById('modal_container5');
- const close5 = document.getElementById('close5');
- 
- open5.addEventListener('click', () => {
-   modal_container5.classList.add('show');  
- });
- 
- close5.addEventListener('click', () => {
-   modal_container5.classList.remove('show');
- });
- 
- 
- //Modal 6
- const open6 = document.getElementById('open6');
- const modal_container6 = document.getElementById('modal_container6');
- const close6 = document.getElementById('close6');
- 
- open6.addEventListener('click', () => {
-   modal_container6.classList.add('show');  
- });
- 
- close6.addEventListener('click', () => {
-   modal_container6.classList.remove('show');
- });
-
-
-  //Modal 7
-  const open7 = document.getElementById('open7');
-  const modal_container7 = document.getElementById('modal_container7');
-  const close7 = document.getElementById('close7');
-  
-  open7.addEventListener('click', () => {
-    modal_container7.classList.add('show');  
-  });
-  
-  close7.addEventListener('click', () => {
-    modal_container7.classList.remove('show');
-  });
-
-
-    //Modal 8
-    const open8 = document.getElementById('open8');
-    const modal_container8 = document.getElementById('modal_container8');
-    const close8 = document.getElementById('close8');
-    
-    open8.addEventListener('click', () => {
-      modal_container8.classList.add('show');  
+    open.addEventListener('click', () => {
+      modal_container.classList.add('show');
     });
-    
-    close8.addEventListener('click', () => {
-      modal_container8.classList.remove('show');
-    });
-    
 
+    close.addEventListener('click', () => {
+      modal_container.classList.remove('show');
+    });
+  }
+}
 
-    //Modal 9
-    const open9 = document.getElementById('open9');
-    const modal_container9 = document.getElementById('modal_container9');
-    const close9 = document.getElementById('close9');
-    
-    open9.addEventListener('click', () => {
-      modal_container9.classList.add('show');  
-    });
-    
-    close9.addEventListener('click', () => {
-      modal_container9.classList.remove('show');
-    });
-    
-
-    //Modal 10
-    const open10 = document.getElementById('open10');
-    const modal_container10 = document.getElementById('modal_container10');
-    const close10 = document.getElementById('close10');
-    
-    open10.addEventListener('click', () => {
-      modal_container10.classList.add('show');  
-    });
-    
-    close10.addEventListener('click', () => {
-      modal_container10.classList.remove('show');
-    });
-    
-  
-  
- 
+generateModalListeners();
