43b97a0177566944d5ccd5eaba7233a3f0b433aa
10Gaby <galvisgabriela1@gmail.com>
2023-04-10 00:24:34 -0500

    si se pudo, tuve fe, quiero dedicar este exito a mi y a todas las mi que estuvieron presentes

--- js/script-infografía.js
+++ js/script-infografía.js
@@ -18,7 +18,7 @@ generateModalListeners();
 
 
 function generateModalListeners2() {
-  for (let i = 1; i <= 30; i++) {
+  for (let i = 1; i <= 10; i++) {
     const open = document.getElementById(`open2-${i}`);
     const modal_container = document.getElementById(`modal_container2-${i}`);
     const close = document.getElementById(`close2-${i}`);
@@ -75,3 +75,9 @@ container.addEventListener("touchmove", function(event) {
 });
                            
 */
+
+
+document.querySelector('.outer-wrapper').addEventListener('scroll', function(event){
+  var letrero = document.querySelector(".letrero");
+  letrero.classList.toggle("animate",event.target.scrollTop>3000)
+})
